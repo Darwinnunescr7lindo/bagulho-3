d133154d1f85d8675dec42159b5c702795bea2eb
Darwinnunescr7lindo <00001137986062SP@al.educacao.sp.gov.br>
2024-11-13 16:16:16 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,11 @@
 criaCartao(
     'Brawl stars',
     'Qual é o melhor brawler?',
-    'A Meg é considerada uma das Brawlers mais fortes por causa do seu Super, que a transforma em um robô poderoso e resistente. No modo robô, ela causa muito mais dano e tem alta resistência, permitindo que ela controle áreas e lide com inimigos de frente. A facilidade em carregar o Super e alternar entre sua forma normal e robótica a torna uma ameaça constante em vários modos de jogo, especialmente em *Showdown* e *Zona Estratégica*.'
+    'A Meg é considerada uma das Brawlers mais fortes por causa do seu Super.'
 )
 
 criaCartao(
-    'Brawl stars',
+  'Brawl stars',
     'skins mais bonitas do Brawl stars',
     'Mecha Crow e Mecha Gold Crow,Lobisomem Leon,Senhor das Trevas Spike,Heroína Bibi,Mecha Dourado Bo,Dragão Rosa e Dragão Verde,Patinho Leon,Fênix Crow'
 )
